e4ba8d8b65bb08328fea358f42b42f317e3ba358
ajazgar <asia.jazgar@gmail.com>
2018-01-23 21:13:27 +0100

    lists of all

--- routes/index.js
+++ routes/index.js
@@ -56,61 +56,175 @@ router.get('/checkIfUsernameExists', function(req, res, next) {
 // });
 
 
-// router.get('/listofdoctors', function(req, res, next) {
-
-// 	if(req.query.userToSearch != undefined){
-//   		var values = [];
-  
-//   		var post  = {login: req.query.userToSearch};
-//   		connection.query('SELECT * FROM lekarz where ?', post, function(err, rows, fields) {
-//             var person = {
-//               'login':rows[0].login
-//             };
-//             	values.push(person);
-//       		res.render('listofusers', {"values": values, "login": req.query.login});
-//   		});
-// 	}
-// 	else{
-//   		var values = [];
-//   		connection.query('SELECT * FROM lekarz', function(err, rows, fields) {
-//         	for (var i in rows) {
-//           		var person = {
-//             		'login':rows[i].login,
-//             		'Specjalizacja':rows[i].email
-//           		};
-//           		values.push(person);
-//       		}
-
-//       		res.render('listofusers', {"values": values, "login": req.query.login});
-//   		});
-// 	}
-  
-// });
-
-//localhost:3000/listofappointments
-router.get('/listofappointments', function(req, res, next) {
-  res.render('listofappointments');
-});
-
-//localhost:3000/listofdoctors
 router.get('/listofdoctors', function(req, res, next) {
-  res.render('listofdoctors');
+
+	if(req.query.userToSearch != undefined){
+  		var values = [];
+  
+  		var post  = {login: req.query.userToSearch};
+  		connection.query('SELECT * FROM lekarz where ?', post, function(err, rows, fields) {
+            var person = {
+              'login':rows[0].login
+            };
+            	values.push(person);
+      		res.render('listofdoctors', {"values": values, "login": req.query.login});
+  		});
+	}
+	else{
+  		var values = [];
+  		connection.query('SELECT * FROM lekarz', function(err, rows, fields) {
+        	for (var i in rows) {
+          		var person = {
+          			'idLekarza':rows[i].idLekarza,
+            		'login':rows[i].login,
+            		'Specjalizacja':rows[i].Specjalizacja
+          		};
+          		values.push(person);
+      		}
+
+      		res.render('listofdoctors', {"values": values, "login": req.query.login});
+  		});
+	}
+  
 });
 
-//localhost:3000/listofpatients
 router.get('/listofpatients', function(req, res, next) {
-  res.render('listofpatients');
+
+	if(req.query.userToSearch != undefined){
+  		var values = [];
+  
+  		var post  = {login: req.query.userToSearch};
+  		connection.query('SELECT * FROM pacjent where ?', post, function(err, rows, fields) {
+            var person = {
+              'login':rows[0].login
+            };
+            	values.push(person);
+      		res.render('listofpatients', {"values": values, "login": req.query.login});
+  		});
+	}
+	else{
+  		var values = [];
+  		connection.query('SELECT * FROM pacjent', function(err, rows, fields) {
+        	for (var i in rows) {
+          		var person = {
+          			'idPacjenta':rows[i].idPacjenta,
+            		'login':rows[i].login,
+            		'nrUbezpieczenia':rows[i].nrUbezpieczenia
+          		};
+          		values.push(person);
+      		}
+
+      		res.render('listofpatients', {"values": values, "login": req.query.login});
+  		});
+	}
+  
 });
 
-//localhost:3000/listofreceipts
+router.get('/listofappointments', function(req, res, next) {
+
+	if(req.query.userToSearch != undefined){
+  		var values = [];
+  
+  		var post  = {idWizyty: req.query.userToSearch};
+  		connection.query('SELECT * FROM wizyta where ?', post, function(err, rows, fields) {
+            var person = {
+              'idWizyty':rows[0].idWizyty
+            };
+            	values.push(person);
+      		res.render('listofappointments', {"values": values, "idWizyty": req.query.idWizyty});
+  		});
+	}
+	else{
+  		var values = [];
+  		connection.query('SELECT * FROM wizyta', function(err, rows, fields) {
+        	for (var i in rows) {
+          		var person = {
+            		'idWizyty':rows[i].idWizyty,
+            		'Data':rows[i].Data,
+            		'Domowa':rows[i].Domowa,
+            		'Platna':rows[i].Platna,
+            		'idLekarza':rows[i].idLekarza,
+            		'idPacjenta':rows[i].idPacjenta
+          		};
+          		values.push(person);
+      		}
+
+      		res.render('listofappointments', {"values": values, "idWizyty": req.query.idWizyty});
+  		});
+	}
+  
+});
+
 router.get('/listofreceipts', function(req, res, next) {
-  res.render('listofreceipts');
+
+	if(req.query.userToSearch != undefined){
+  		var values = [];
+  
+  		var post  = {idRecepty: req.query.userToSearch};
+  		connection.query('SELECT * FROM recepta where ?', post, function(err, rows, fields) {
+            var person = {
+              'idRecepty':rows[0].idRecepty
+            };
+            	values.push(person);
+      		res.render('listofreceipts', {"values": values, "idRecepty": req.query.idRecepty});
+  		});
+	}
+	else{
+  		var values = [];
+  		connection.query('SELECT * FROM recepta', function(err, rows, fields) {
+        	for (var i in rows) {
+          		var person = {
+            		'idRecepty':rows[i].idRecepty,
+            		'idChoroby':rows[i].idChoroby,
+            		'Refundacja':rows[i].Refundacja,
+            		'idPacjenta':rows[i].idPacjenta,
+            		'idLekarza':rows[i].idLekarza,
+            		'idWizyty':rows[i].idWizyty
+          		};
+          		values.push(person);
+      		}
+
+      		res.render('listofreceipts', {"values": values, "idRecepty": req.query.idRecepty});
+  		});
+	}
+  
 });
 
-//localhost:3000/listofreferrals
+
 router.get('/listofreferrals', function(req, res, next) {
-  res.render('listofreferrals');
+
+	if(req.query.userToSearch != undefined){
+  		var values = [];
+  
+  		var post  = {idSkierowania: req.query.userToSearch};
+  		connection.query('SELECT * FROM skierowanie where ?', post, function(err, rows, fields) {
+            var person = {
+              'idSkierowania':rows[0].idSkierowania
+            };
+            	values.push(person);
+      		res.render('listofreferrals', {"values": values, "idSkierowania": req.query.idSkierowania});
+  		});
+	}
+	else{
+  		var values = [];
+  		connection.query('SELECT * FROM skierowanie', function(err, rows, fields) {
+        	for (var i in rows) {
+          		var person = {
+            		'idSkierowania':rows[i].idSkierowania,
+            		'idPacjenta':rows[i].idPacjenta,
+            		'idLekarza':rows[i].idLekarza,
+            		'idWizyty':rows[i].idWizyty
+          		};
+          		values.push(person);
+      		}
+
+      		res.render('listofreferrals', {"values": values, "idSkierowania": req.query.idSkierowania});
+  		});
+	}
+  
 });
+
+
 
 
 module.exports = router;
