6ebc5f6bb4c2d2d84aa8003bd6e2afa8d716aba2
ajazgar <asia.jazgar@gmail.com>
2018-01-23 13:27:55 +0100

    example routing

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,30 @@ router.get('/panel', function(req, res, next) {
   res.render('panel');
 });
 
+//localhost:3000/listofappointments
+router.get('/listofappointments', function(req, res, next) {
+  res.render('listofappointments');
+});
+
+//localhost:3000/listofdoctors
+router.get('/listofdoctors', function(req, res, next) {
+  res.render('listofdoctors');
+});
+
+//localhost:3000/listofpatients
+router.get('/listofpatients', function(req, res, next) {
+  res.render('listofpatients');
+});
+
+//localhost:3000/listofreceipts
+router.get('/listofreceipts', function(req, res, next) {
+  res.render('listofreceipts');
+});
+
+//localhost:3000/listofreferrals
+router.get('/listofreferrals', function(req, res, next) {
+  res.render('listofreferrals');
+});
+
 
 module.exports = router;
